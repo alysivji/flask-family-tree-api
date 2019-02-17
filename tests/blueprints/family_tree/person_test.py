f9b4db0d817a6a551326f4db1513cef75373293f
Aly Sivji <alysivji@gmail.com>
2019-02-16 21:40:51 -0600

    add test for get list endpoint

--- tests/blueprints/family_tree/person_test.py
+++ tests/blueprints/family_tree/person_test.py
@@ -1,3 +1,19 @@
+def test_get_list_of_people(client, session):
+    for i in range(5):
+        test_person = {
+            "first_name": "Test",
+            "last_name": "User",
+            "email": f"test{i}@user.com",
+        }
+        client.post("/api/person", json=test_person)
+
+    rv = client.get("/api/person")
+
+    result = rv.json["data"]
+    assert rv.status_code == 200
+    assert len(result) == 5
+
+
 def test_create_person(client, session):
     test_person = {
         "first_name": "Test",
@@ -6,7 +22,7 @@ def test_create_person(client, session):
     }
 
     rv = client.post("/api/person", json=test_person)
-    result = rv.json["data"]
 
+    result = rv.json["data"]
     assert rv.status_code == 201
     assert result["id"] == 1
